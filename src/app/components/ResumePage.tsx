c960d7769bc37e16239823b4c37e5b0365a165a2
Peaerson Grant <pearsongrant23@gmail.com>
2026-01-22 16:17:10 +0200

    correct

--- src/app/components/ResumePage.tsx
+++ src/app/components/ResumePage.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-//import "./ResumePage.scss";
 import { motion } from "framer-motion";
 
 const ResumePage: React.FC = () => {
@@ -7,7 +6,7 @@ const ResumePage: React.FC = () => {
     <section className="resume-page">
       <div className="resume-container">
 
-        // Header 
+        {/* // Header  */}
         <motion.div
           className="resume-header"
           initial={{ opacity: 0, y: 30 }}
