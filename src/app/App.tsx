28af10a653c8c6eef99e93abdec1b1c572c78b39
Peaerson Grant <pearsongrant23@gmail.com>
2026-01-22 19:40:01 +0200

    correct

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,11 @@
-// import React, {useEffect} from 'react';
+
 import RoutePath from './routes/RoutePath.tsx';
 import './assets/App.scss';
 import { BrowserRouter } from 'react-router-dom';
-// import Home from './components/Home.tsx';
 import Header from './components/header/Navbar.tsx';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
-
+import { Analytics } from "@vercel/analytics/react"
 
 const App = () => {
     
@@ -14,11 +13,8 @@ const App = () => {
     
       <BrowserRouter>
           <Header/>
-          {/* <Header/ */}
-          {/* <Route path = '/' element={() => <Home/>} /> */}
-          <RoutePath />
           
-          {/* <Footer/> */}
+          <RoutePath />         
         
           </BrowserRouter>
     
