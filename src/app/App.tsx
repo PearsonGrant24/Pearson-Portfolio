45b9442e47255e8782758340a70afbb24ed7baa4
Peaerson Grant <pearsongrant23@gmail.com>
2026-01-22 19:43:28 +0200

    correct

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,25 +1,19 @@
-
 import RoutePath from './routes/RoutePath.tsx';
 import './assets/App.scss';
 import { BrowserRouter } from 'react-router-dom';
 import Header from './components/header/Navbar.tsx';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
-import { Analytics } from "@vercel/analytics/react"
+import { Analytics } from "@vercel/analytics/react";
 
 const App = () => {
-    
-    return (
-    
-      <BrowserRouter>
-          <Header/>
-          
-          <RoutePath />         
-        
-          </BrowserRouter>
-    
+  return (
+    <BrowserRouter>
+      <Header />
+      <RoutePath />
+      <Analytics />
+    </BrowserRouter>
   );
-  
-}
+};
 
 export default App;
