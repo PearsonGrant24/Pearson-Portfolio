c24c4bfaa64e3f696500609495e7d0e9e18578e6
Peaerson Grant <pearsongrant23@gmail.com>
2026-01-22 15:08:14 +0200

    correct

--- src/app/routes/RoutePath.tsx
+++ src/app/routes/RoutePath.tsx
@@ -2,14 +2,14 @@
 import { Routes,  Route } from 'react-router-dom';
 // const Home = lazy(() => import("../components/pages/Home.tsx"));
 import Home from '../components/Home.tsx';
-// import About from '../components/About.tsx';
-// import GetinTouch from '../components/GetinTouch.tsx';
-// import ResumePage from '../components/ResumePage.tsx';
-// import HireMePage from '../components/HireMePage.tsx';
-// //import ContactPage from '../components/ContactPage.tsx';
-// import ProjectsPage from '../components/ProjectsPage.tsx';
-// import ProjectDetails from '../components/CaseStudies/ProjectDetails.tsx'
-// import AdminDashboard from '../components/AdminDashboard.tsx'
+import About from '../components/About.tsx';
+import GetinTouch from '../components/GetinTouch.tsx';
+import ResumePage from '../components/ResumePage.tsx';
+import HireMePage from '../components/HireMePage.tsx';
+//import ContactPage from '../components/ContactPage.tsx';
+import ProjectsPage from '../components/ProjectsPage.tsx';
+import ProjectDetails from '../components/CaseStudies/ProjectDetails.tsx'
+import AdminDashboard from '../components/AdminDashboard.tsx'
 
 const RoutePath = () => {
  
@@ -18,13 +18,13 @@ const RoutePath = () => {
 		<Routes>
         <Route path="/" element={<Home />} />
 		<Route path="/Home" element={<Home />} />
-		{/* <Route path="/About" element={<About />} />
+		<Route path="/About" element={<About />} />
 		<Route path="/ProjectsPage" element={<ProjectsPage />} />
 		<Route path="/CaseStudies/:id" element={<ProjectDetails />} />
 		<Route path="/HireMePage" element={<HireMePage/>} />
-		<Route path="/ContactPage" element={<ContactPage/>} />
+		{/* <Route path="/ContactPage" element={<ContactPage/>} /> */}
 		<Route path="/ResumePage" element={<ResumePage/>} />
-		<Route path="/AdminDashboard" element={<AdminDashboard/>} /> */}
+		<Route path="/AdminDashboard" element={<AdminDashboard/>} />
 
         </Routes>
 	
