fa39ffbff994ff5cbc3a48a481942eb13c3bc429
Peaerson Grant <pearsongrant23@gmail.com>
2026-01-22 13:26:39 +0200

    all

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,8 +1,8 @@
-import React, {useEffect} from 'react';
+// import React, {useEffect} from 'react';
 import RoutePath from './routes/RoutePath.tsx';
 import './assets/App.scss';
-import { BrowserRouter, Route } from 'react-router-dom';
-import Home from './components/Home.tsx';
+import { BrowserRouter } from 'react-router-dom';
+// import Home from './components/Home.tsx';
 import Header from './components/header/Navbar.tsx';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
--- src/app/routes/RoutePath.tsx
+++ src/app/routes/RoutePath.tsx
@@ -1,5 +1,5 @@
-import React, { Suspense, lazy } from 'react';
-import { Routes,  Route, Router } from 'react-router-dom';
+// import React, { Suspense, lazy } from 'react';
+import { Routes,  Route } from 'react-router-dom';
 // const Home = lazy(() => import("../components/pages/Home.tsx"));
 import Home from '../components/Home.tsx';
 import About from '../components/About.tsx';
